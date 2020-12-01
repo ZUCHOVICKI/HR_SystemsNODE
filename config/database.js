6b1e43571798131d282a83759ee075e196e50902
Emiliano Zuchovicki <emilianozuchovicki@hotmail.com>
2020-12-01 12:27:32 -0600

    Config final

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const util = require('util')
 const pool = mysql.createPool({
 
     connectionLimit:10,
-    host: 'CLEARDB_DATABASE_URL',
+    host: 'us-cdbr-east-02.cleardb.com',
     user: 'b0ee6cf985588e',
     password: '460c8def',
     database: 'heroku_df4d2f3e87a3c2b'
@@ -14,3 +14,4 @@ pool.query = util.promisify(pool.query)
 
 
 module.exports = pool
+
